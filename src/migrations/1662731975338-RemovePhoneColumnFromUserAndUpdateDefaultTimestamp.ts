89a409c3085072d35ca74239c5c8fc7492d5040f
kol007 <koldev007@gmail.com>
2025-01-11 17:56:44 +0200

    update migrations to mysql

--- src/migrations/1662731975338-RemovePhoneColumnFromUserAndUpdateDefaultTimestamp.ts
+++ src/migrations/1662731975338-RemovePhoneColumnFromUserAndUpdateDefaultTimestamp.ts
@@ -6,36 +6,38 @@ export class RemovePhoneColumnFromUserAndUpdateDefaultTimestamp1662731975338
   name = 'RemovePhoneColumnFromUserAndUpdateDefaultTimestamp1662731975338';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "phone_number"`);
     await queryRunner.query(
-      `ALTER TABLE "user" ALTER COLUMN "created_at" SET DEFAULT now()`,
+      `ALTER TABLE \`user\` DROP COLUMN \`phone_number\``,
     );
     await queryRunner.query(
-      `ALTER TABLE "user" ALTER COLUMN "updated_at" SET DEFAULT now()`,
+      `ALTER TABLE \`user\` CHANGE \`created_at\` \`created_at\` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP`,
     );
     await queryRunner.query(
-      `ALTER TABLE "cart" ALTER COLUMN "created_at" SET DEFAULT now()`,
+      `ALTER TABLE \`user\` CHANGE \`updated_at\` \`updated_at\` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`,
     );
     await queryRunner.query(
-      `ALTER TABLE "cart" ALTER COLUMN "updated_at" SET DEFAULT now()`,
+      `ALTER TABLE \`cart\` CHANGE \`created_at\` \`created_at\` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`cart\` CHANGE \`updated_at\` \`updated_at\` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "cart" ALTER COLUMN "updated_at" SET DEFAULT '2022-09-09 13:32:59.291'`,
+      `ALTER TABLE \`cart\` CHANGE \`updated_at\` \`updated_at\` TIMESTAMP NOT NULL DEFAULT '2022-09-09 13:32:59.291'`,
     );
     await queryRunner.query(
-      `ALTER TABLE "cart" ALTER COLUMN "created_at" SET DEFAULT '2022-09-09 13:32:59.291'`,
+      `ALTER TABLE \`cart\` CHANGE \`created_at\` \`created_at\` TIMESTAMP NOT NULL DEFAULT '2022-09-09 13:32:59.291'`,
     );
     await queryRunner.query(
-      `ALTER TABLE "user" ALTER COLUMN "updated_at" SET DEFAULT '2022-09-09 13:32:59.29'`,
+      `ALTER TABLE \`user\` CHANGE \`updated_at\` \`updated_at\` TIMESTAMP NOT NULL DEFAULT '2022-09-09 13:32:59.29'`,
     );
     await queryRunner.query(
-      `ALTER TABLE "user" ALTER COLUMN "created_at" SET DEFAULT '2022-09-09 13:32:59.29'`,
+      `ALTER TABLE \`user\` CHANGE \`created_at\` \`created_at\` TIMESTAMP NOT NULL DEFAULT '2022-09-09 13:32:59.29'`,
     );
     await queryRunner.query(
-      `ALTER TABLE "user" ADD "phone_number" character varying NOT NULL`,
+      `ALTER TABLE \`user\` ADD \`phone_number\` VARCHAR(255) NOT NULL`,
     );
   }
 }
